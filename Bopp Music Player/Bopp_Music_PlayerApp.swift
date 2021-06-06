67701802d5d098f6d7e87a4d48e0f7100293e3a3
mohanaji <mohanaji@sheridancollege.ca>
2021-06-06 05:54:29 -0400

    Initial Commit

---
+++ Bopp Music Player/Bopp_Music_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bopp_Music_PlayerApp.swift
+//  Bopp Music Player
+//
+//  Created by Najim Mohammed on 2021-06-06.
+//
+
+import SwiftUI
+
+@main
+struct Bopp_Music_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
